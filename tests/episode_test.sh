75105e76083f74c1e69fa088d406f76481ebccdc
Michael Borde <michael.borde@gmail.com>
2014-10-08 23:06:59 +0200

    Refactorings

--- tests/episode_test.sh
+++ tests/episode_test.sh
@@ -1,25 +1,29 @@
 source "sources/episode.sh"
 
 function le_chemin_du_soustitre_est_le_meme_que_l_episode_a_lextension_pres() {
-  local episode="un_chemin/episode-s1e2.avi"
+  local episode="chemin/episode-s1e2.avi"
 
   local soustitre="$(episode__chemin_soustitre "${episode}")"
 
-  assertion__equal "un_chemin/episode-s1e2.srt" "${soustitre}"
+  assertion__equal "chemin/episode-s1e2.srt" "${soustitre}"
 }
 
 function un_soustitre_correspond_a_un_episode_sil_a_le_meme_nom() {
-  assertion__successful episode__correspondent "episode-s1e2.avi" "episode-s1e2.srt"
+  assertion__successful \
+    episode__correspondent "episode-s1e2.avi" "episode-s1e2.srt"
 }
 
-function un_soustitre_correspond_a_un_episode_sil_a_le_meme_nom_bien_que_dans_un_autre_repertoire() {
-  assertion__successful episode__correspondent "episode/episode-s1e2.avi" "soustitre/episode-s1e2.srt"
+function un_soustitre_peut_correspondre_a_lepisode_dun_autre_repertoire() {
+  assertion__successful \
+    episode__correspondent "episode/episode-s1e2.avi" "st/episode-s1e2.srt"
 }
 
 function _un_soustitre_correspond_a_un_episode_sil_a_le_meme_numero_depisode() {
-  assertion__successful episode__correspondent "episode-s1e2.avi" "episode-s1x2.srt"
+  assertion__successful \
+    episode__correspondent "episode-s1e2.avi" "episode-s1x2.srt"
 }
 
 function un_soustitre_qui_na_rien_a_voir_ne_correspond_pas_a_un_episode() {
-  assertion__failing episode__correspondent "episode-s1e2.avi" "episode-s1e10.srt"
+  assertion__failing \
+    episode__correspondent "episode-s1e2.avi" "episode-s1e10.srt"
 }
