2809a7ab461c5d8657652dab9e8709ecb734d6d4
Michael Borde <michael.borde@gmail.com>
2014-10-12 17:43:41 +0200

    Refacto et consolidation regex numéro

--- sources/episode.sh
+++ sources/episode.sh
@@ -1,14 +1,16 @@
-function episode__chemin_soustitre() {
+function episode__soustitre_final() {
   local chemin_episode=$1
   echo "${chemin_episode%\.*}.srt"
 }
 
 function episode__correspondent() {
-  local numero_episode="$(_numero_episode_depuis_nom_fichier "$1")"
-  local numero_soustitre="$(_numero_episode_depuis_nom_fichier "$2")"
+  local numero_episode="$(_numero_episode_depuis_fichier "$1")"
+  local numero_soustitre="$(_numero_episode_depuis_fichier "$2")"
   [[ "${numero_episode}" == "${numero_soustitre}" ]]
 }
 
-function _numero_episode_depuis_nom_fichier() {
-  echo "$1" | sed "s/.*\([0-9]\)\{1,2\}.\([0-9]\)\{1,2\}.*/\1e\2/"
+function _numero_episode_depuis_fichier() {
+  echo "$1" \
+    | grep -o "[0-9]\{1,\}[^0-9][0-9]\{1,\}" \
+    | sed "s/0*\([0-9]*\)[^0-9]0*\([0-9]*\)/\1e\2/"
 }
--- tests/episode_test.sh
+++ tests/episode_test.sh
@@ -1,34 +1,33 @@
 source "sources/episode.sh"
 
-function le_chemin_du_st_est_le_meme_que_l_episode_a_lextension_pres() {
-  local episode="chemin/episode-s1e2.avi"
-
-  local soustitre="$(episode__chemin_soustitre "${episode}")"
-
-  assertion__equal "chemin/episode-s1e2.srt" "${soustitre}"
-}
-
-function un_st_correspond_a_un_episode_sil_a_le_meme_nom() {
+function un_soustitre_correspond_sil_a_le_meme_nom_que_lepisode() {
   assertion__successful \
-    episode__correspondent "episode-s1e2.avi" "episode-s1e2.srt"
+    episode__correspondent "ma.serie.s01e04.avi" "ma.serie.s01e04.srt"
 }
 
-function un_st_peut_correspondre_a_un_episode_dans_un_autre_repertoire() {
+function un_soustitre_peut_correspondre_quimporte_son_chemin() {
   assertion__successful \
-    episode__correspondent "episode/episode-s1e2.avi" "st/episode-s1e2.srt"
+    episode__correspondent "1/ma.serie.s01e04.avi" "a/b/c/ma.serie.s01e04.srt"
 }
 
-function un_st_correspond_a_un_episode_sil_a_le_meme_numero_depisode() {
+function un_soustitre_peut_correspondre_quimporte_le_format_de_lepisode() {
   assertion__successful \
-    episode__correspondent "episode-s1e2.avi" "episode-s1x2.srt"
-}
-
-function un_st_peut_correspondre_a_un_episode_au_format_tres_different() {
+    episode__correspondent "ma.serie.s01e04.avi" "ma.serie.1x04.srt"
   assertion__successful \
-    episode__correspondent "episode-s1e2.avi" "mon.episode.01e02.hi.srt"
+    episode__correspondent "ma.serie.s01e04.avi" "ma.serie.1x4.srt"
+  assertion__successful \
+    episode__correspondent "ma.serie.s300e004.avi" "ma.serie.300x04.srt"
 }
 
-function un_st_qui_na_rien_a_voir_ne_correspond_pas_a_un_episode() {
+function un_soustitre_peut_ne_pas_correspond_du_tout() {
   assertion__failing \
-    episode__correspondent "episode-s1e2.avi" "episode-s1e10.srt"
+    episode__correspondent "ma.serie.s301e104.avi" "ma.serie.1e4.srt"
+}
+
+function le_chemin_du_soustitre_final_est_le_meme_que_lepisode() {
+  local episode="mes/episodes/ma.serie.s01e12.avi"
+
+  local soustitre="$(episode__soustitre_final "${episode}")"
+
+  assertion__equal "mes/episodes/ma.serie.s01e12.srt" "${soustitre}"
 }
